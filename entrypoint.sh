a9e2db3e0ba7f42964b9b8b69190ce5154225f4d
YIN JIA YI <jiayi@iscas.ac.cn>
2021-03-10 09:54:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,9 +39,9 @@ EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+sh -c "aws s3 cp ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
-              --no-progress \
+              --no-progress --recursive \
               ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
